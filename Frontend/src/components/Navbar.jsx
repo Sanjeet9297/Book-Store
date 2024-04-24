63348e9040b6b2cef6c15d4bfecba27558dbe042
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-24 10:55:36 +0530

    add more changes

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
+  const [sticky , setSticky] = useState(false)
+  useEffect(() => {
+    const handleScrollY = () => {
+    if(window.scroll>0){
+      setSticky(true)
+    }
+    else{
+      setSticky(false)
+    }
+  }
+    window.removeEventListener("scroll",handleScrollY)
+    return() => {
+      window.removeEventListener("scroll", handleScrollY)
+    }
+  }, [])
+
   const navItems = (
     <>
       <li>
@@ -22,7 +38,10 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div className={` max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+        sticky
+        ?"sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out" : ""}`}>
+
         <div className="navbar bg-base-100">
           <div className="navbar-start">
             <div className="dropdown">
