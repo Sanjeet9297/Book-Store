069c5a1dd27d179a7ef92b57779c0bb4da017235
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-25 10:26:35 +0530

    add new changes

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import list from "../../public/list.json"
+
 const Freebook = () => {
+  const filterData = list.filter((data) => data.category === "Free");
+  console.log(filterData);
   return (
-    <div>Freebook</div>
-  )
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <h1 className="font-semibold text-xl pb-2">Free Offered Course</h1>
+        <p>
+          lorem, ispum dolar sit amet consectetur adipisicing elite. Dolar, et
+          totam. Tempora amet atque expedita, quae corrupti total sed particular
+          corporis at veniam est voluptas animi!
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Freebook
