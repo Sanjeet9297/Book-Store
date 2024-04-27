ce488a20cd8fc4565a0aebd1b1d091cc3de828e7
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-27 10:09:11 +0530

    add more changes

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-    <hr/>
-      <footer className="footer footer-center p-10 text-base-content rounded">
+      <hr />
+      <footer className="footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white">
         <nav className="grid grid-flow-col gap-4">
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
