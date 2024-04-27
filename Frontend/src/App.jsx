ae8c000ea4883507d654745f32fb36afe3e43f2b
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-27 12:14:55 +0530

    add more functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Home from "./home/Home";
 import { Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import Contact from "./components/Contact";
 
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Courses />} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </>
